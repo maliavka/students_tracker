b82460a3996efaaea35a71365fd87dc2a19764c5
maliavka <marchenko.tetyana@gmail.com>
2020-01-05 10:31:09 +0200

    HW 29/12/2019

--- src/students/models.py
+++ src/students/models.py
@@ -1,5 +1,8 @@
+import random
+
 from datetime import datetime
 from django.db import models
+from faker import Faker
 
 
 '''
@@ -19,14 +22,46 @@ class Student(models.Model):
     def get_info(self):
         return f'{self.first_name}{self.last_name}{self.birth_date}'
 
-
     @classmethod
     def generate_student(cls):
         student = cls(
             first_name='D',
             last_name='K',
-            birth_date=...,
+            birth_date=datetime.now().date(),
             email='hfgydfgy@gmail.com',
             telephone='1234566',
         )
         student.save()
+
+    @classmethod
+    def generate_fake_student(cls):
+        fake = Faker()
+        student = cls(
+            first_name=fake.name(),
+            last_name=((fake.name()).split())[1],
+            birth_date=datetime.now().date(),
+            email=fake.email(),
+            telephone=(random.choice(int) for i in range(7)),
+        )
+        student.save()
+
+
+    @classmethod
+    def generate_100_fake_student(cls):
+        fake = Faker()
+        for i in range(100):
+            student = cls(
+                first_name=fake.name(),
+                last_name=((fake.name()).split())[1],
+                birth_date=datetime.now().date(),
+                email=fake.email(),
+                telephone=(random.choice(int) for i in range(7)),
+            )
+            student.save()
+
+
+class Group(models.Model):
+    group_name = models.CharField(max_length=25)
+    faculty_name = models.CharField(max_length=25)
+    course_name = models.IntegerField()
+    start_date = models.DateField()
