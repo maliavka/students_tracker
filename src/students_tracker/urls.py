5bab829da95bec8527150bffd11e9ec29ca63138
maliavka <marchenko.tetyana@gmail.com>
2020-01-19 11:07:17 +0200

    19/01/2020_2nd_part

--- src/students_tracker/urls.py
+++ src/students_tracker/urls.py
@@ -15,10 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('stud_and_groups/', include('students.urls')),
     path('teachers/', include('teachers.urls')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns=[
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
+
