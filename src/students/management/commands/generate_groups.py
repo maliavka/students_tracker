76a75edf6224f4e3dd4c019ac74fc59a36e5aa55
maliavka <marchenko.tetyana@gmail.com>
2020-01-30 01:15:18 +0200

    30/01/2020_HW_lesson11

--- src/students/management/commands/generate_groups.py
+++ src/students/management/commands/generate_groups.py
@@ -1,5 +1,10 @@
+import random
+
 from django.core.management.base import BaseCommand
-from students.models import Group
+from faker import Faker
+from datetime import datetime
+from students.models import Student, Group
+from teachers.models import Teacher
 
 
 class Command(BaseCommand):
@@ -13,6 +18,34 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        number = int(options.get('number') or 50)
+        Group.objects.all().delete()
+        Student.objects.all().delete()
+        Teacher.objects.all().delete()
+        fake = Faker()
+
+        teachers = [Teacher.objects.create(
+            teach_name=f'teacher_{j}',
+            first_name=fake.first_name(),
+            last_name=fake.last_name(),
+            birth_date=fake.date_of_birth(tzinfo=None, minimum_age=25,maximum_age=65),
+            position=fake.job(),
+            email=fake.email(),
+            telephone=fake.phone_number(),
+        ) for j in range(50)]
+
+        students = [Student.objects.create(
+            st_name=f'student_{i}',
+            first_name=fake.first_name(),
+            last_name=fake.last_name(),
+            birth_date=datetime.now().date(),
+            email=fake.email(),
+            telephone=fake.phone_number(),
+
+        ) for i in range(100)]
+
+        number = int(options.get('number') or 10)
         for _ in range(number):
-            Group.generate_group()
+            group = Group.generate_group()
+            group.headman = random.choice(students)
+            group.curator = random.choice(teachers)
+            group.save()
--- src/teachers/models.py
+++ src/teachers/models.py
@@ -4,13 +4,14 @@ from faker import Faker
 
 
 class Teacher(models.Model):
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-    birth_date = models.DateField()
-    position = models.CharField(max_length=50)
+    teach_name = models.CharField(max_length=50, null=True, blank=True)
+    first_name = models.CharField(max_length=20, blank=True, default=None)
+    last_name = models.CharField(max_length=20, blank=True, default=None)
+    birth_date = models.DateField(null=True, blank=True, default=None)
+    position = models.CharField(max_length=50, blank=True, default=None)
     email = models.EmailField()
     # add avatar TODO
-    telephone = models.CharField(max_length=16)  # clean phone TODO
+    telephone = models.CharField(max_length=30, blank=True, default=None)  # clean phone TODO
 
     def get_info(self):
         return f'{self.first_name} ' \
